ccae71aca46ffbacac2478dae2bbc5e51dcc6c22
yarchiee <yachi98159@gmail.com>
2022-10-26 19:12:28 +0800

    fix app

--- src/06.Compoenet Test/01/ComponentTest.test.js
+++ src/06.Compoenet Test/01/ComponentTest.test.js
@@ -1,6 +1,6 @@
+import App from "../../App";
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import App from "../App";
 
 it("renders welcome message", () => {
   render(<App />);
